494f0d81d421082dddc1e34b25fc66ae55f5ebfe
Michael Haynes <antihaynes@gmail.com>
2023-07-18 09:09:33 -0500

    Finish longestValidParens

--- javascript/longestValidParens.js
+++ javascript/longestValidParens.js
@@ -3,6 +3,37 @@
  * @return {number}
  */
 var longestValidParentheses = function (s) {
-  let partialSums = s.split("").map(x => x === "(" ? 1 : -1)
-    .reduce((diffs, next) => [...diffs, diffs.at(-1) + next], [0]);
+  let stack = [];
+  let maxLength = 0;
+  let adjacent = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(i);
+    } else if (s[i] === ")") {
+      if (stack.length) {
+        maxLength = Math.max(maxLength, i - stack.pop() + 1 + adjacent);
+        adjacent = maxLength;
+      } else {
+        adjacent = 0;
+      }
+    }
+  }
+  return maxLength;
 };
+
+var longestValidPrefix = function (s) {
+  let counter = 0;
+  let longestPrefix = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      counter++;
+    } else if (s[i] === ")") {
+      counter--;
+    }
+    if (counter < 0)
+      break;
+    if (counter === 0)
+      longestPrefix = i + 1;
+  }
+  return longestPrefix;
+}
