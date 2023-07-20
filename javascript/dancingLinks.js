9a1c0b461ee05c9b0733654ca65b04e0804cf7bc
Michael Haynes <antihaynes@gmail.com>
2023-07-20 12:03:20 -0500

    [WIP] Finish first-pass Sudoku solver

--- javascript/dancingLinks.js
+++ javascript/dancingLinks.js
@@ -11,121 +11,144 @@ var CircularListNodeFactory = function (...circularRefNames) {
   }
 }
 
-class SparseBooleanMatrix {
-  static RANDOMIZATION_PROBABILITY = 0.3;
-  // static MatrixElem(left = null, right = null, up = null, down = null, header=null) {
-  //   ret = { left, right, up, down, header };
-  //   if (!ret.left) ret.left = ret;
-  //   if (!ret.right) ret.right = ret;
-  //   if (!ret.up) ret.up = ret;
-  //   if (!ret.down) ret.down = ret;
-  //   return ret;
-  // }
-  static ListNode = CircularListNodeFactory("left", "right", "up", "down", "header");
-  static removeColumn(node) {
-    node.left.right = node.right;
-    node.right.left = node.left;
-  }
-  static removeRow(node) {
-    node.up.down = node.down;
-    node.down.up = node.up;
-  }
-  static restoreColumn(node) {
-    node.left.right = node;
-    node.right.left = node;
-  }
-  static restoreRow(node) {
-    node.up.down = node;
-    node.down.up = node;
-  }
-  addColumnHeader(colName) {
-    let header = SparseBooleanMatrix.ListNode({
-      left: this.columns?.left,
-      right: this.columns,
-      name: colName,
-      count: 0
-    });
-    this.columns = this.columns ?? header;
-    this.columns.left.right = header;
-    this.columns.left = header;
-  }
-  addElem(rowName, colName) {
-    let elem = SparseBooleanMatrix.ListNode({
-      left: this.rowsByName[rowName]?.left,
-      right: this.rowsByName[rowName],
-      header: this.rowsByName[rowName]?.left?.header ?? this.columns
-    });
-    while (elem.header.name < colName) {
-      elem.header = elem.header.right;
-    }
-    this.rowsByName[rowName] = this.rowsByName[rowName] ?? elem;
-    elem.up = elem.header.up;
-    elem.down = elem.header;
-    this.restoreRow(elem);
-  }
-  constructor(colNames, rowData) {
-    colNames.sort();
-    this.rowDataByName = {};
-    for (let colName of colNames)
-      this.addColumnHeader(colName);
-    for (let [rowName, ...cols] of rowData) {
-      cols.sort();                    // so that we iterate in the same order as before
-      for (let colName of cols) {
-        this.addElem(rowName, colName);
-      }
-    }
-    // Stack for removed rowData to enable backtracking
-    this.solution = [];
+class ConstraintMatrix {
+
+  constructor(randomizationProbability = 0.3) {
+    // Used for nondeterministically choosing rows
+    this.RANDOMIZATION_PROBABILITY = randomizationProbability;
+    this._solution = [];
+    this._rows = [];
+    this._columns = null;
   }
 
-  *getRowIterator(node) {
+  static ListNode = CircularListNodeFactory("left", "right", "up", "down", "header");
+  static Possibility = (id, [...constraints]) => { return { id, constraints, name: toString(id) } };
+
+  static _removeColumn(node) {
+    if (node?.left) node.left.right = node.right;
+    if (node?.right) node.right.left = node.left;
+  }
+
+  static _removeRow(node) {
+    if (node?.up) node.up.down = node.down;
+    if (node?.down) node.down.up = node.up;
+  }
+
+  static _restoreColumn(node) {
+    if (node?.left) node.left.right = node;
+    if (node?.right) node.right.left = node;
+  }
+
+  static _restoreRow(node) {
+    if (node?.up) node.up.down = node;
+    if (node?.down) node.down.up = node;
+  }
+
+  addConstraints(...ids) {
+    let node;
+    let right = this._columns;
+    for (let name of ids.map(toString).sort()) {
+      for (; right && right.name < name; right = right?.right) { }
+      this._restoreColumn(node = ConstraintMatrix.ListNode({
+        name,
+        right,
+        left: right?.left,
+        count: 0
+      }));
+      this._columns = this._columns && this._columns.name <= node.name ? this._columns : node;
+      right = node;
+    }
+  }
+
+  addPossibility(data, [...colIds]) {
+    let idx = this._rows.push(data) - 1;
+    let colHeader = this._columns;
+    let tail;
+    let node;
+    for (let colName of colIds.map(toString).sort()) {
+      for (; colHeader.name < colName; colHeader = colHeader.right) {
+        if (colHeader.name !== colName) {
+          colHeader = ConstraintMatrix.ListNode({
+            left: colHeader?.left,
+            right: colHeader?.right,
+            name: colName,
+            count: 0
+          });
+          this._restoreRow(colHeader);
+        }
+        node = ConstraintMatrix.ListNode({
+          left: tail,
+          right: tail?.right,
+          header: colHeader,
+          rowIdx: idx,
+          up: header.up,
+          down: header
+        });
+        this._restoreRow(node);
+        this._restoreColumn(node);
+        header.count++;
+      }
+    }
+  }
+
+  *_getElemRowIterator(node) {
     yield node;
     for (let curr = node.right; curr !== node; curr = curr.right)
       yield curr;
   }
 
-  *getColumnIterator(node) {
+  *_getElemColumnIterator(node) {
     yield node;
     for (let curr = node.down; curr !== node; curr = curr.down)
       yield curr;
   }
 
-  getRandomColElem(col) {
-    for (let curr = col.head; Math.random() > SparseBooleanMatrix.RANDOMIZATION_PROBABILITY; curr = curr.down) { }
+  _getRandomColElem(col) {
+    for (let curr = col.head; Math.random() > this.RANDOMIZATION_PROBABILITY || curr === col.head; curr = curr.down) { }
     return curr
   }
 
-  backtrack() {
-    let node = this.solution.pop();
-    for (let colNode of this.getColumnIterator(node)) {
-      this.restoreRow(colNode);
-      for (let rowNode of this.getRowIterator(colNode)) {
-        this.restoreColumn(rowNode);
+  _getNextColumn() {
+    let min = this._columns;
+    for (let header of this._getElemRowIterator(this._columns)) {
+      min = min && min.count < header.count ? min : header;
+    }
+    return min;
+  }
+
+  _backtrack() {
+    let node = this._solution.pop();
+    for (let colNode of this._getElemColumnIterator(node)) {
+      this._restoreRow(colNode);
+      for (let rowNode of this._getElemRowIterator(colNode)) {
+        this._restoreColumn(rowNode);
       }
     }
   }
 
-  dancingLinks() {
+  solve() {
+    // Solution to the exact cover problem represented by the Boolean constraint matrix using Knuth's "Dancing Links" algorithm
     while (true) {
-      let col = this.getNextColumn();
+      let col = this._getNextColumn();
       if (col.up === col) {
         // Empty column
         if (col.right === col) {
           console.log("Great success!  Dancing Links has yielded a solution to all your problems");
-          return;
+          return this._solution.map(n => this._rows[n.rowIdx]);
         } else {
-          this.backtrack();
+          this._backtrack();
         }
       } else {
-        let elem = this.getRandomColElem(col);
-        this.solution.push(elem);
-        for (let rowNode of this.getRowIterator(elem)) {
-          for (let colNode of this.getColumnIterator(rowNode)) {
-            this.removeRow(colNode);
+        let elem = this._getRandomColElem(col);
+        this._solution.push(elem);
+        for (let rowNode of this._getElemRowIterator(elem)) {
+          for (let colNode of this._getElemColumnIterator(rowNode)) {
+            this._removeRow(colNode);
           }
-          this.removeColumn(rowNode);
+          this._removeColumn(rowNode);
         }
       }
     }
   }
 }
+
