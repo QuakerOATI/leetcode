aba0bfc93b3c7fffc4b7db48e09c48201856f251
Michael Haynes <antihaynes@gmail.com>
2023-07-28 13:18:30 -0500

    Solve alien_order

--- python/alien_order.py
+++ python/alien_order.py
@@ -13,12 +13,38 @@ If this claim is incorrect, and the given arrangement of string in words cannot 
 Otherwise, return a string of the unique letters in the new alien language sorted in lexicographically increasing order by the new language's rules. If there are multiple solutions, return any of them.
 """
 from typing import List
+from collections import Counter
 
 
 class Solution:
 
     def alienOrder(self, words: List[str]) -> str:
         """
-        Topological sort?
+        Topological sort
+
+        Accepted, with optimal runtime!
         """
-        ...
+        edges = {}
+        priors = Counter(set.union(*map(set, words)))
+        order = ""
+        for w1, w2 in zip(words, words[1:]):
+            # print(w1, w2)
+            if w1.startswith(w2) and w1 != w2:
+                return ""
+            for c1, c2 in zip(w1, w2):
+                # print(f"    {(c1, c2)}")
+                if c1 != c2:
+                    edges.setdefault(c1, set())
+                    if c2 not in edges[c1]:
+                        edges[c1].add(c2)
+                        priors[c2] += 1
+                    break
+        while len(priors) > 0:
+            # print(priors, edges)
+            if priors.pop(last := min(priors, key=lambda k: priors[k])) > 1:
+                return ""
+            # print(f"    Last: {last}")
+            order += last
+            for c in edges.get(last, []):
+                priors[c] -= 1
+        return order
