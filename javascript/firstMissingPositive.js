891c462846afee2d0b7c53ee0b78f0303cf3c04d
Michael Haynes <antihaynes@gmail.com>
2023-07-31 07:42:33 -0500

    [WIP] firstMissingPositive

---
+++ javascript/firstMissingPositive.js
@@ -0,0 +1,11 @@
+// Given an unsorted integer array nums, return the smallest missing positive integer.
+//
+// You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+
+};
