0cae4cbbb95c0e2dfbc088bc619d1d3bbcfaf833
Michael Haynes <Michael.Haynes@oati.net>
2023-08-01 08:35:23 -0500

    Solve fairCookieDistribution

--- javascript/fairCookieDistribution.js
+++ javascript/fairCookieDistribution.js
@@ -3,7 +3,26 @@
  * @param {number} k
  * @return {number}
  */
+
 var distributeCookies = function(cookies, k) {
+    var helper = function(children, cookies) {
+        if (cookies.length === 0)
+            return Math.max(...children);
+        let c = cookies.pop();
+        let best = Infinity;
+        for (let i=0; i<children.length; ++i) {
+            children[i] += c;
+            best = Math.min(best, helper(children, cookies));
+            children[i] -= c;
+        }
+        cookies.push(c);
+        return best;
+    }
+    return helper(Array(k).fill(0), cookies.sort());
+};
+
+// Wrong answer: greedy algorithm is incorrect here.
+var distributeCookiesGreedy = function(cookies, k) {
     cookies.sort((x, y) => y - x);
     if (k === 1)
         return cookies.reduce((S, c) => S + c, 0);
@@ -21,34 +40,34 @@ var distributeCookies = function(cookies, k) {
 function Heap(l, cmp) {
     this.arr = l;
     this.cmp = cmp;
-}
+};
 
 Heap.prototype.upheap = function(idx) {
     for (let p=Math.floor((idx-1)/2); p>=0 && this.cmp(this.arr[p], this.arr[idx]) > 0; [idx, p] = [p, Math.floor((p-1)/2)]) {
         [this.arr[p], this.arr[idx]] = [this.arr[idx], this.arr[p]];
     }
     return idx;
-}
+};
 
 Heap.prototype.minChild = function(idx) {
     return [2*idx+1, 2*idx+2].filter(x => x<this.arr.length).sort((i, j) => this.cmp(this.arr[i], this.arr[j]))[0];
-}
+};
 
 Heap.prototype.downheap = function(idx) {
     for (let c=this.minChild(idx); c && this.cmp(this.arr[c], this.arr[idx]) < 0; [idx, c] = [c, this.minChild(c)]) {
         [this.arr[c], this.arr[idx]] = [this.arr[idx], this.arr[c]];
     }
-}
+};
 
 Heap.prototype.insert = function(x) {
     this.arr.push(x);
     let i = this.upheap(this.arr.length-1);
     this.downheap(i);
-}
+};
 
 Heap.prototype.removeMin = function() {
     [this.arr[0], this.arr[this.arr.length-1]] = [this.arr[this.arr.length-1], this.arr[0]];
     let ret = this.arr.pop();
     this.downheap(0);
     return ret;
-}
+};
