9f3055cd8372fa96edac2acbdbbb06c9c74b01b3
Michael Haynes <Michael.Haynes@oati.net>
2023-08-02 18:09:23 -0500

    [WIP] More work on firstMissingPositive

--- javascript/firstMissingPositive.js
+++ javascript/firstMissingPositive.js
@@ -7,5 +7,20 @@
  * @return {number}
  */
 var firstMissingPositive = function (nums) {
+    let digits = Array(32).fill(0);
+    let sums = Array(32).fill(0);
+    let masks = [...Array(32).keys()].map(i => (1 << (i+1)) - 1);
+    let [m, M] = [Infinity, 0];
+    for (let num of nums.filter(x => x > 0)) {
+        [m, M] = [Math.min(m, num), Math.max(M, num)];
+        masks.forEach((m, i) => {
+            sums[i] += (m & num); 
+            digits[i] += (1 + (m>>1)) & num;
+        });
+    }
+    if (m > 1)
+        return 1;
+    
+
 
 };
