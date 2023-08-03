cf3103b4078756195c41adced12c35a7625aaf78
Michael Haynes <Michael.Haynes@oati.net>
2023-08-03 09:56:45 -0500

    Finish firstMissingPositive

--- javascript/firstMissingPositive.js
+++ javascript/firstMissingPositive.js
@@ -6,7 +6,80 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive = function (nums) {
+var firstMissingPositive = function(nums) {
+    // 95 %ile runtime
+    // 73.92 %ile memory
+    var swap = function(arr, i, j) {
+        if (i === j)
+            return;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    };
+
+    for (let i=0; i<nums.length; ++i) {
+        while (nums[i] > 0 && nums[i] <= nums.length && nums[i] !== nums[nums[i]-1]) {
+            swap(nums, i, nums[i]-1);
+        }
+    }
+
+    let first = 0;
+    while (nums[first++] === first);
+    return first;
+}
+
+// Useful implementation of partitioning, but too involved for this problem
+var firstMissingPositive = function(nums) {
+    var swap = function(arr, i, j) {
+        if (i === j)
+            return;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    };
+
+    var partitionLeft = function(arr, l, r, p) {
+        let [ml, Ml] = [p, -Infinity];
+        var updateRange = function(n) {
+            [ml, Ml] = [Math.min(ml, n), Math.max(Ml, n)];
+        }
+        console.log(`Partitioning array slice [${arr.slice(l, r+1)}] on pivot ${p}...`);
+        for (; arr[r] > p; r--);
+        for (; arr[l] <= p; l++) updateRange(arr[l]);
+        for (let i=l; i<=r; ++i) {
+            if (arr[i] <= p) {
+                console.log(`Swapping ${arr[i]} at index ${i} with ${arr[l]} at index ${l}`);
+                console.log(`   Incrementing l = ${l} --> ${l+1}...`);
+                updateRange(arr[i]);
+                swap(arr, i, l++);
+                for (; arr[l] <= p; l++) updateRange(arr[l]);
+            } else {
+                console.log(`Swapping ${arr[i]} at index ${i} with ${arr[r]} at index ${r}`);
+                console.log(`   Decrementing r = ${r} --> ${r-1}...`);
+                swap(arr, i, r--);
+                updateRange(arr[i]);
+                for (; arr[r] > p; r--);
+            }
+            console.log(`State at end of iteration:\n\tl = ${l}\n\tr = ${r}\n\tarr = [${arr}]`);
+        }
+        return {pivot: r, min: ml, max: Ml};
+    }
+
+    var fmpHelper = function(arr, l, r) {
+        // not clear what to do from here
+        let m = partition(arr, l, r, Math.floor((r - l + 1)/2));
+    }
+
+    let l = partition(nums, 0, nums.length-1, 0) + 1;
+    console.log(`Array after first partition:\n\t[${nums.slice(0, l)} ; ${nums[l]} ; ${nums.slice(l+1)}]`);
+    if (l >= nums.length-1)
+        return 1;
+    for (let i=0; i<nums.length; ++i) {
+        if (nums[i] === next) {
+            [nums[i], nums[idx]] = [nums[idx], nums[i]];
+            idx++;
+            next++;
+        }
+}
+
+// Half-baked and probably going nowhere
+var firstMissingPositiveBinaryBuckets = function (nums) {
     let digits = Array(32).fill(0);
     let sums = Array(32).fill(0);
     let masks = [...Array(32).keys()].map(i => (1 << (i+1)) - 1);
@@ -20,7 +93,4 @@ var firstMissingPositive = function (nums) {
     }
     if (m > 1)
         return 1;
-    
-
-
 };
