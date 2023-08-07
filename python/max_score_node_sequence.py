a1087c519c52c360d3137395ccae6fe9d5e22c3b
Michael Haynes <antihaynes@gmail.com>
2023-08-07 08:37:04 -0500

    Solve max_score_node_sequence

--- python/max_score_node_sequence.py
+++ python/max_score_node_sequence.py
@@ -1,6 +1,22 @@
 from typing import List
+from bisect import bisect_left
+
 
 class Solution:
-    def maximumScore(self, scores: List[int], edges: List[List[int]]) -> int:
-        best = -1
 
+    def maximumScore(self, scores: List[int], edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(len(scores))]
+        for a, b in edges:
+            adj[a].insert(bisect_left(adj[a], (scores[b], b)), (scores[b], b))
+            adj[b].insert(bisect_left(adj[b], (scores[a], a)), (scores[a], a))
+
+        best = -1
+        for a, b in edges:
+            try:
+                best_outside = max(sc + sd for sc, c in adj[a][-3:]
+                                   for sd, d in adj[b][-3:]
+                                   if c != b and d != a and c != d)
+                best = max(best, scores[a] + scores[b] + best_outside)
+            except ValueError:
+                continue
+        return best
