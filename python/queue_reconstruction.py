c5585fab1b7f95def9cfeecf8570d6fc17d8fa55
Michael Haynes <antihaynes@gmail.com>
2023-08-10 00:18:48 -0500

    [WIP] queue_reconstruction

--- python/queue_reconstruction.py
+++ python/queue_reconstruction.py
@@ -3,6 +3,45 @@ You are given an array of people, people, which are the attributes of some peopl
 
 Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
 """
+from typing import List
+
 
 class Solution:
+
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+
+        def swap(arr, i, j):
+            arr[i], arr[j] = arr[j], arr[i]
+            redir[i] =
+
+        idx = list(range(len(people)))
+        redir = {}
+        people.sort()
+        prev, degeneracy = -1, 1
+        for i in range(len(people)):
+            print("------------------")
+            f, b = redir.get(i, [i, i])
+            ht, rk = people[f]
+            print(
+                f"prev = {prev}, ht = {ht}, rd = {rk}, degeneracy = {degeneracy}")
+            if ht == prev:
+                rk -= degeneracy
+                degeneracy += 1
+            else:
+                degeneracy = 1
+            prev = ht
+            print(f"{i} --> redir({i}) = [{f}, {b}]")
+            print(f"rk = people[f][1] - degeneracy = {rk}")
+            print(f"people = {people}")
+            print(f"idx = {idx}")
+            # print(f"idx[people[{f}][1]] = {idx[people[f][1]]}")
+            # print("------------------")
+            swap(people, f, j := idx.pop(rk))
+            print(f"f = {f}, i = {i}, j = {j}, b = {b}")
+            print(redir)
+            redir[j] = [f, i]
+            redir[i] = [j, b]
+            redir[f][1] = j
+            redir[b][0] =
+            print(redir)
+        return people
