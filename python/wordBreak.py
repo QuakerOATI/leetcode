75bdad1e5e27fded56438b538d61d6b9c777a8bd
Michael Haynes <antihaynes@gmail.com>
2023-08-16 23:17:22 -0500

    Solve wordBreak

--- python/wordBreak.py
+++ python/wordBreak.py
@@ -4,6 +4,14 @@ Given a string s and a dictionary of strings wordDict, return true if s can be s
 Note that the same word in the dictionary may be reused multiple times in the segmentation.
 """
 
+
 class Solution:
+
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        while len(s) 
+        wds = set(wordDict)
+        dp = [0] * (len(s) + 1)
+        for i, c in enumerate(s):
+            for w in wds:
+                if s[i:].startswith(w):
+                    dp[i + len(w)] = max(dp[i + len(w)], dp[i] + len(w))
+        return dp[-1] == len(s)
