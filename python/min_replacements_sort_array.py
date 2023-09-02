65b9c1bd78f3b74c10f9fa4a39de79f7479df493
Michael Haynes <antihaynes@gmail.com>
2023-09-02 17:20:37 -0500

    Solve min_replacements_sort_array

---
+++ python/min_replacements_sort_array.py
@@ -0,0 +1,20 @@
+"""
+You are given a 0-indexed integer array nums. In one operation you can replace any element of the array with any two elements that sum to it.
+
+    For example, consider nums = [5,6,7]. In one operation, we can replace nums[1] with 2 and 4 and convert nums to [5,2,4,7].
+
+Return the minimum number of operations to make an array that is sorted in non-decreasing order.
+"""
+
+from math import ceil
+
+
+class Solution:
+
+    def minimumReplacement(self, nums: List[int]) -> int:
+        m = float("inf")
+        numSteps = 0
+        for num in reversed(nums):
+            numSteps += ceil(num / m) - 1
+            m = min(m, num)
+        return numSteps
